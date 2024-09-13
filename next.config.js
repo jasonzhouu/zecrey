280c6c7d817eea82a7cd40cb2dbd166486df8d09
wangyafei <wyfblues@163.com>
2024-09-13 12:28:21 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+`;
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
